77653726dcdd179b173e1af2bd50aabf5326c5fe
nooralndr <142223850+nooralndr@users.noreply.github.com>
2023-09-11 13:23:49 +0300

    Loop created for the game

--- src/arvauspeli.java
+++ src/arvauspeli.java
@@ -9,19 +9,24 @@ public class arvauspeli {
 		
 		String answer1;
 
-		
+		do
+		{
 		System.out.println("What is my name?");
 		answer1 = in.nextLine();
 
 		if (answer1.equals("Noora"))
 		{
 			System.out.println("Congratulations! You guessed right.");
+			break;
 		}
 		
 		else 
 		{
 			System.out.println("Wrong! Guess again.");
 		}
+		}
+		while (true);
+		
 		
 	}
 
