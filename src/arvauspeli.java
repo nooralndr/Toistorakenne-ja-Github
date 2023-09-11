cd7fa584dec1ad82b252cec7a94f7315e8e4580c
nooralndr <142223850+nooralndr@users.noreply.github.com>
2023-09-11 13:01:43 +0300

    First part of game - guessing my name

--- src/arvauspeli.java
+++ src/arvauspeli.java
@@ -1,9 +1,28 @@
 
+import java.util.Scanner;
+
 public class arvauspeli {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Scanner in = new Scanner(System.in);
+		
+		String answer1;
 
+		
+		System.out.println("What is my name?");
+		answer1 = in.nextLine();
+
+		if (answer1.equals("Noora"))
+		{
+			System.out.println("Congratulations! You guessed right.");
+		}
+		
+		else 
+		{
+			System.out.println("Wrong! Guess again.");
+		}
+		
 	}
 
 }
